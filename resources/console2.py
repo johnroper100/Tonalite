69b6e83864bad9042d73ec590d86a2532f05d8a4
John Roper <johnroper100@gmail.com>
2019-06-28 19:09:25 -0400

    cleanup

--- resources/console2.py
+++ resources/console2.py
@@ -93,10 +93,10 @@ def changeChanValue(chan, direction):
     chan = 0
     if direction == 1:
         if currentFixtureChans['parameters'][chan]['value'] < currentFixtureChans['parameters'][chan]['max']:
-            currentFixtureChans['parameters'][chan]['value'] = currentFixtureChans['parameters'][chan]['value'] + 1
+            currentFixtureChans['parameters'][chan]['value'] = currentFixtureChans['parameters'][chan]['value'] + 255
     elif direction == -1:
         if currentFixtureChans['parameters'][chan]['value'] > 0:
-            currentFixtureChans['parameters'][chan]['value'] = currentFixtureChans['parameters'][chan]['value'] - 1
+            currentFixtureChans['parameters'][chan]['value'] = currentFixtureChans['parameters'][chan]['value'] - 255
     sio.emit('changeFixtureParameterValue', {
              'id': fixtures[0]['id'], 'pid': chan, 'value': currentFixtureChans['parameters'][chan]['value']})
 
