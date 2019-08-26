1c9ba027ae9b0f4cb5e4c00eb10d7ee74ce71bb1
John Roper <johnroper100@gmail.com>
2019-08-26 19:22:43 -0400

    working on grid layout

--- tonalite-ui/index.js
+++ tonalite-ui/index.js
@@ -1,19 +1,24 @@
+var testLayout = [
+    { "x": 0, "y": 0, "w": 1, "h": 1, "i": "0", selected: false },
+    { "x": 5, "y": 5, "w": 1, "h": 1, "i": "1", selected: false },
+    { "x": 2, "y": 12, "w": 1, "h": 1, "i": "2", selected: false },
+    { "x": 7, "y": 6, "w": 1, "h": 1, "i": "3", selected: false },
+    { "x": 10, "y": 8, "w": 1, "h": 1, "i": "4", selected: false },
+    { "x": 5, "y": 0, "w": 1, "h": 1, "i": "5", selected: false }
+];
 var app = new Vue({
     el: '#app',
     data: {
         currentTab: 'fixtures',
         fixturesDisplay: 'fixtures',
-        fixtures: [{ name: "hi", selected: false, id: "123" }],
-        selectedFixtures: []
+        fixtures: testLayout,
+        layoutMode: false
     },
     methods: {
-        selectFixture: function (fixtureID) {
-            if (app.fixtures[app.fixtures.map(el => el.id).indexOf(fixtureID)].selected == false) {
-                app.selectedFixtures.push(fixtureID);
-            } else {
-                app.selectedFixtures.splice(app.selectedFixtures.indexOf(fixtureID), 1);
+        selectItem: function (item) {
+            if (app.layoutMode == false) {
+                item.selected = !item.selected;
             }
-            app.fixtures[app.fixtures.map(el => el.id).indexOf(fixtureID)].selected = !app.fixtures[app.fixtures.map(el => el.id).indexOf(fixtureID)].selected;
         }
     }
 });
