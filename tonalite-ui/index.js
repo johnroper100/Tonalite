c4839c2c5c2e313d7220ea593f32804d5751320d
John Roper <johnroper100@gmail.com>
2019-08-27 22:37:31 -0400

    add groups and group selection

--- tonalite-ui/index.js
+++ tonalite-ui/index.js
@@ -1,10 +1,10 @@
 var testLayout = [
-    { "x": 0, "y": 0, "w": 1, "h": 1, "i": "0", "name": "Martin Mac 360", "universe": 0, "coarse": 1, channels: ["hi"]},
-    { "x": 1, "y": 0, "w": 1, "h": 1, "i": "1", "name": "Martin Mac 360", "universe": 0, "coarse": 2, channels: ["hi"]},
-    { "x": 2, "y": 0, "w": 1, "h": 1, "i": "2", "name": "Martin Mac 360", "universe": 0, "coarse": 3, channels: ["hi"]},
-    { "x": 3, "y": 0, "w": 1, "h": 1, "i": "3", "name": "Martin Mac 360", "universe": 0, "coarse": 4, channels: ["hi"]},
-    { "x": 4, "y": 0, "w": 1, "h": 1, "i": "4", "name": "Martin Mac 360", "universe": 0, "coarse": 5, channels: ["hi"]},
-    { "x": 5, "y": 0, "w": 1, "h": 1, "i": "5", "name": "Martin Mac 360", "universe": 0, "coarse": 6, channels: ["hi"]}
+    { "x": 0, "y": 0, "w": 1, "h": 1, "i": "0", "name": "Martin Mac 360", "universe": 0, "coarse": 1, channels: ["hi"] },
+    { "x": 1, "y": 0, "w": 1, "h": 1, "i": "1", "name": "Martin Mac 360", "universe": 0, "coarse": 2, channels: ["hi"] },
+    { "x": 2, "y": 0, "w": 1, "h": 1, "i": "2", "name": "Martin Mac 360", "universe": 0, "coarse": 3, channels: ["hi"] },
+    { "x": 3, "y": 0, "w": 1, "h": 1, "i": "3", "name": "Martin Mac 360", "universe": 0, "coarse": 4, channels: ["hi"] },
+    { "x": 4, "y": 0, "w": 1, "h": 1, "i": "4", "name": "Martin Mac 360", "universe": 0, "coarse": 5, channels: ["hi"] },
+    { "x": 5, "y": 0, "w": 1, "h": 1, "i": "5", "name": "Martin Mac 360", "universe": 0, "coarse": 6, channels: ["hi"] }
 ];
 var app = new Vue({
     el: '#app',
@@ -17,9 +17,9 @@ var app = new Vue({
         layoutMode: false,
         cues: [],
         groups: [
-            {"name": "Test group", "i": "0"},
-            {"name": "Test group", "i": "1"},
-            {"name": "Test group", "i": "2"}
+            { "name": "Test group", "i": "0" },
+            { "name": "Test group", "i": "1" },
+            { "name": "Test group", "i": "2" }
         ],
         presets: []
     },
@@ -34,11 +34,11 @@ var app = new Vue({
             }
         },
         selectGroup: function (groupID) {
-                if (app.selectedGroups.indexOf(groupID) >= 0) {
-                    app.selectedGroups.splice(app.selectedGroups.indexOf(groupID), 1);
-                } else {
-                    app.selectedGroups.push(groupID);
-                }
+            if (app.selectedGroups.indexOf(groupID) >= 0) {
+                app.selectedGroups.splice(app.selectedGroups.indexOf(groupID), 1);
+            } else {
+                app.selectedGroups.push(groupID);
+            }
         },
         setLayoutMode: function (value) {
             app.layoutMode = value;
@@ -46,6 +46,9 @@ var app = new Vue({
         },
         deselectAllFixtures: function () {
             app.selectedFixtures = [];
+        },
+        deselectAllGroups: function () {
+            app.selectedGroups = [];
         }
     }
 });
