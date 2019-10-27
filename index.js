1f3e14cea76e36b8a227bec292deec40026b5ab2
John Roper <johnroper100@gmail.com>
2019-10-27 17:32:46 -0400

    start work on hsl

--- index.js
+++ index.js
@@ -1194,7 +1194,7 @@ io.on('connection', function (socket) {
 
                 if (fixture.colortable == "3874B444-A11E-47D9-8295-04556EAEBEA7") {
                     // RGB
-                    colortable = JSON.parse(JSON.stringify(require(process.cwd() + "/chips/rgb.json")));
+                    colortable = JSON.parse(JSON.stringify(require(process.cwd() + "/chips.json")));
                     let col = 0; const colMax = colortable.length; for (; col < colMax; col++) {
                         color = { color: colortable[col].color, parameters: [] };
 
@@ -1206,7 +1206,7 @@ io.on('connection', function (socket) {
                     }
                 } else if (fixture.colortable == "77A82F8A-9B24-4C3F-98FC-B6A29FB1AAE6") {
                     // RGBW
-                    colortable = JSON.parse(JSON.stringify(require(process.cwd() + "/chips/rgb.json")));
+                    colortable = JSON.parse(JSON.stringify(require(process.cwd() + "/chips.json")));
                     let col = 0; const colMax = colortable.length; for (; col < colMax; col++) {
                         color = { color: colortable[col].color, parameters: [] };
 
@@ -1221,7 +1221,7 @@ io.on('connection', function (socket) {
                     }
                 } else if (fixture.colortable == "D3E71EC8-3406-4572-A64C-52A38649C795") {
                     // RGBA
-                    colortable = JSON.parse(JSON.stringify(require(process.cwd() + "/chips/rgb.json")));
+                    colortable = JSON.parse(JSON.stringify(require(process.cwd() + "/chips.json")));
                     let col = 0; const colMax = colortable.length; for (; col < colMax; col++) {
                         color = { color: colortable[col].color, parameters: [] };
 
@@ -1237,7 +1237,7 @@ io.on('connection', function (socket) {
                     }
                 } else if (fixture.colortable == "C7A1FB0A-AA23-468F-9060-AC1625155DE8") {
                     // RGBAW
-                    colortable = JSON.parse(JSON.stringify(require(process.cwd() + "/chips/rgb.json")));
+                    colortable = JSON.parse(JSON.stringify(require(process.cwd() + "/chips.json")));
                     let col = 0; const colMax = colortable.length; for (; col < colMax; col++) {
                         color = { color: colortable[col].color, parameters: [] };
 
@@ -1254,7 +1254,7 @@ io.on('connection', function (socket) {
                     }
                 } else if (fixture.colortable == "EF4970BA-2536-4725-9B0F-B2D7A021E139") {
                     // CMY
-                    colortable = JSON.parse(JSON.stringify(require(process.cwd() + "/chips/rgb.json")));
+                    colortable = JSON.parse(JSON.stringify(require(process.cwd() + "/chips.json")));
                     let col = 0; const colMax = colortable.length; for (; col < colMax; col++) {
                         color = { color: colortable[col].color, parameters: [] };
 
@@ -1264,7 +1264,33 @@ io.on('connection', function (socket) {
 
                         fixture.chips.push(color);
                     }
-                }
+                } /*else if (fixture.colortable == "B074A2D3-0C40-45A7-844A-7C2721E0B267") {
+                    // HSI
+                    colortable = JSON.parse(JSON.stringify(require(process.cwd() + "/chips.json")));
+                    let col = 0; const colMax = colortable.length; for (; col < colMax; col++) {
+                        color = { color: colortable[col].color, parameters: [] };
+
+                        r = colortable[col].parameters[0];
+                        g = colortable[col].parameters[1];
+                        b = colortable[col].parameters[2];
+                        i = (r + g + b) / 3.0;
+                        rn = r / (r + g + b);
+                        gn = g / (r + g + b);
+                        bn = b / (r + g + b);
+                        h = Math.acos((0.5 * ((rn - gn) + (rn - bn))) / (Math.sqrt((rn - gn) * (rn - gn) + (rn - bn) * (gn - bn))));
+                        if (b > g) {
+                            h = 2 * Math.PI - h;
+                        }
+
+                        s = 1 - 3 * Math.min(rn, Math.min(gn, bn));
+
+                        color.parameters.push({ name: "Hue", value: h });
+                        color.parameters.push({ name: "Saturation", value: s });
+                        color.parameters.push({ name: "Intensity", value: i });
+
+                        fixture.chips.push(color);
+                    }
+                }*/
                 let c = 0; const cMax = fixture.parameters.length; for (; c < cMax; c++) {
                     fixture.parameters[c].value = fixture.parameters[c].home;
                     fixture.parameters[c].max = 65535;
