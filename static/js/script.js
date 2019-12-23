f79c9f5f5c8929cd35d9f5537f2d3014ad41b9c7
John Roper <johnroper100@gmail.com>
2019-12-23 15:28:33 -0500

    cleanup

--- static/js/script.js
+++ static/js/script.js
@@ -148,7 +148,7 @@ var app = new Vue({
             }
         },
         ifMobile: function () {
-            return isMobile.any && isMobile.other.chrome == false;
+            return isMobile.any;
         },
         resetShow: function () {
             bootbox.confirm("Are you sure you want a new show? This will reset everything.", function (result) {
