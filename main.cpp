1be0a57f78f3b6546c8270c03158f9d9a1d7d54d
John Roper <johnroper100@gmail.com>
2020-10-18 16:43:32 -0400

    only use 4 universes

--- main.cpp
+++ main.cpp
@@ -62,19 +62,11 @@ unordered_map<string, Fixture> fixtures;
 
 bool SendData(ola::client::OlaClientWrapper *wrapper)
 {
-    static uint8_t frames[6144] = {0};
+    static uint8_t frames[2048] = {0};
     static ola::DmxBuffer buffer1;
     static ola::DmxBuffer buffer2;
-    static ola::DmxBuffer buffer3;
+    static ola::DmxBuffer buffer3; 
     static ola::DmxBuffer buffer4;
-    static ola::DmxBuffer buffer5;
-    static ola::DmxBuffer buffer6;
-    static ola::DmxBuffer buffer7;
-    static ola::DmxBuffer buffer8;
-    static ola::DmxBuffer buffer9;
-    static ola::DmxBuffer buffer10;
-    static ola::DmxBuffer buffer11;
-    static ola::DmxBuffer buffer12;
 
     lock_guard<mutex> lg(door);
     for (auto &it : fixtures)
@@ -91,41 +83,17 @@ bool SendData(ola::client::OlaClientWrapper *wrapper)
     buffer2.Blackout();
     buffer3.Blackout();
     buffer4.Blackout();
-    buffer5.Blackout();
-    buffer6.Blackout();
-    buffer7.Blackout();
-    buffer8.Blackout();
-    buffer9.Blackout();
-    buffer10.Blackout();
-    buffer11.Blackout();
-    buffer12.Blackout();
     for (int ii = 0; ii < 512; ii++)
     {
         buffer1.SetChannel(ii, frames[(0 * 512) + ii]);
         buffer2.SetChannel(ii, frames[(1 * 512) + ii]);
         buffer3.SetChannel(ii, frames[(2 * 512) + ii]);
         buffer4.SetChannel(ii, frames[(3 * 512) + ii]);
-        buffer5.SetChannel(ii, frames[(4 * 512) + ii]);
-        buffer6.SetChannel(ii, frames[(5 * 512) + ii]);
-        buffer7.SetChannel(ii, frames[(6 * 512) + ii]);
-        buffer8.SetChannel(ii, frames[(7 * 512) + ii]);
-        buffer9.SetChannel(ii, frames[(8 * 512) + ii]);
-        buffer10.SetChannel(ii, frames[(9 * 512) + ii]);
-        buffer11.SetChannel(ii, frames[(10 * 512) + ii]);
-        buffer12.SetChannel(ii, frames[(11 * 512) + ii]);
     }
     wrapper->GetClient()->SendDMX(1, buffer1, ola::client::SendDMXArgs());
     wrapper->GetClient()->SendDMX(2, buffer2, ola::client::SendDMXArgs());
     wrapper->GetClient()->SendDMX(3, buffer3, ola::client::SendDMXArgs());
     wrapper->GetClient()->SendDMX(4, buffer4, ola::client::SendDMXArgs());
-    wrapper->GetClient()->SendDMX(5, buffer5, ola::client::SendDMXArgs());
-    wrapper->GetClient()->SendDMX(6, buffer6, ola::client::SendDMXArgs());
-    wrapper->GetClient()->SendDMX(7, buffer7, ola::client::SendDMXArgs());
-    wrapper->GetClient()->SendDMX(8, buffer8, ola::client::SendDMXArgs());
-    wrapper->GetClient()->SendDMX(9, buffer9, ola::client::SendDMXArgs());
-    wrapper->GetClient()->SendDMX(10, buffer10, ola::client::SendDMXArgs());
-    wrapper->GetClient()->SendDMX(11, buffer11, ola::client::SendDMXArgs());
-    wrapper->GetClient()->SendDMX(12, buffer12, ola::client::SendDMXArgs());
 
     if (finished == 1)
     {
--- run.sh
+++ run.sh
@@ -2,26 +2,10 @@ ola_patch -d 2 -p 0 -u 1
 ola_patch -d 2 -p 1 -u 2
 ola_patch -d 2 -p 2 -u 3
 ola_patch -d 2 -p 3 -u 4
-ola_patch -d 2 -p 4 -u 5
-ola_patch -d 2 -p 5 -u 6
-ola_patch -d 2 -p 6 -u 7
-ola_patch -d 2 -p 7 -u 8
-ola_patch -d 2 -p 8 -u 9
-ola_patch -d 2 -p 9 -u 10
-ola_patch -d 2 -p 10 -u 11
-ola_patch -d 2 -p 11 -u 12
 
 ola_patch -d 7 -p 0 -u 1
 ola_patch -d 7 -p 1 -u 2
 ola_patch -d 7 -p 2 -u 3
 ola_patch -d 7 -p 3 -u 4
-ola_patch -d 7 -p 4 -u 5
-ola_patch -d 7 -p 5 -u 6
-ola_patch -d 7 -p 6 -u 7
-ola_patch -d 7 -p 7 -u 8
-ola_patch -d 7 -p 8 -u 9
-ola_patch -d 7 -p 9 -u 10
-ola_patch -d 7 -p 10 -u 11
-ola_patch -d 7 -p 11 -u 12
 
 ./tonalite
