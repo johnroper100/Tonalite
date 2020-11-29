a5b7184f1d138b5da8aded0c8d2fa0f2b97160d8
John Roper <johnroper100@gmail.com>
2020-11-29 12:03:01 -0500

    move into static

---
+++ static/app.js
@@ -0,0 +1,140 @@
+const socket = new WebSocket("ws://" + location.host);
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        fixtureProfiles: {},
+        fixtureProfilesManufacturer: "",
+        fixtureProfilesModel: "",
+        fixtureProfilesSearch: "",
+        fixtureProfileUniverse: 1,
+        fixtureProfileAddress: 1,
+        fixtureProfileNumber: 1,
+        fixturesTab: 'fixturesList',
+        fixtureGridLayout: false,
+        fixtures: [],
+        selectedFixtures: [],
+        tab: "fixtures"
+    },
+    computed: {
+        filteredFixtureProfilesList() {
+            if (this.isEmpty(this.fixtureProfiles) == false) {
+                if (app.fixtureProfilesManufacturer != "") {
+                    if (app.fixtureProfilesModel != "") {
+                        var keys = Object.values(this.fixtureProfiles[app.fixtureProfilesManufacturer][app.fixtureProfilesModel]);
+                        return {
+                            'objs': keys.filter(profile => {
+                                return profile.modeName.toLowerCase().includes(this.fixtureProfilesSearch.toLowerCase());
+                            }), 'type': 'modes'
+                        };
+                    } else {
+                        var keys = Object.keys(this.fixtureProfiles[app.fixtureProfilesManufacturer]);
+                        return {
+                            'objs': keys.filter(profile => {
+                                return profile.toLowerCase().includes(this.fixtureProfilesSearch.toLowerCase());
+                            }), 'type': 'models'
+                        };
+                    }
+                } else {
+                    var keys = Object.keys(this.fixtureProfiles);
+                    return {
+                        'objs': keys.filter(profile => {
+                            return profile.toLowerCase().includes(this.fixtureProfilesSearch.toLowerCase());
+                        }), 'type': 'manufacturers'
+                    };
+                }
+            } else {
+                return { 'objs': [], 'type': 'manufacturers' };
+            }
+        }
+    },
+    methods: {
+        isEmpty: function (obj) {
+            for (var key in obj) {
+                if (obj.hasOwnProperty(key))
+                    return false;
+            }
+            return true;
+        },
+        clearFixtureProfilesSelection: function (type) {
+            if (type == 'manufacturers') {
+                app.fixtureProfilesManufacturer = "";
+                app.fixtureProfilesModel = "";
+            } else if (type == 'models') {
+                app.fixtureProfilesModel = "";
+            }
+            app.fixtureProfilesSearch = "";
+        },
+        setFixtureProfilesSelection: function (obj, type) {
+            if (type == 'manufacturers') {
+                app.fixtureProfilesManufacturer = obj;
+            } else if (type == 'models') {
+                app.fixtureProfilesModel = obj;
+            }
+            app.fixtureProfilesSearch = "";
+        },
+        clearFixtureProfilesSelection: function (type) {
+            if (type == 'manufacturers') {
+                app.fixtureProfilesManufacturer = "";
+                app.fixtureProfilesModel = "";
+            } else if (type == 'models') {
+                app.fixtureProfilesModel = "";
+            }
+            app.fixtureProfilesSearch = "";
+        },
+        selectFixture: function (fixtureID) {
+            if (app.fixtureGridLayout == false) {
+                if (app.selectedFixtures.indexOf(fixtureID) >= 0) {
+                    app.selectedFixtures.splice(app.selectedFixtures.indexOf(fixtureID), 1);
+                } else {
+                    app.selectedFixtures.push(fixtureID);
+                }
+            }
+        },
+        moveFixture: function (fixtureID, x, y) {
+            message = {
+                "msgType": "moveFixture",
+                "i": fixtureID,
+                "x": parseInt(x),
+                "y": parseInt(y)
+            }
+            socket.send(JSON.stringify(message));
+        },
+        resizeFixture: function (fixtureID, h, w, hp, wp) {
+            message = {
+                "msgType": "resizeFixture",
+                "i": fixtureID,
+                "h": parseInt(h),
+                "w": parseInt(w)
+            }
+            socket.send(JSON.stringify(message));
+        },
+        getFixtureProfiles: function () {
+            socket.send(JSON.stringify({ "msgType": "getFixtureProfiles" }));
+            app.fixtureProfilesManufacturer = "";
+            app.fixtureProfilesModel = "";
+            app.fixtureProfilesSearch = "";
+            app.fixtureProfileUniverse = 1;
+            app.fixtureProfileAddress = 1;
+            app.fixtureProfileNumber = 1;
+            $('#fixtureModal').modal('show');
+        }
+    }
+});
+
+socket.addEventListener('message', function (event) {
+    msg = JSON.parse(event.data);
+    if (msg["msgType"] == "fixtures") {
+        app.fixtures = msg["fixtures"];
+    } else if (msg["msgType"] == "moveFixture") {
+        item = app.fixtures.find(x => x.i === msg["i"]);
+        item.x = msg["x"];
+        item.y = msg["y"];
+    } else if (msg["msgType"] == "resizeFixture") {
+        item = app.fixtures.find(x => x.i === msg["i"]);
+        item.w = msg["w"];
+        item.h = msg["h"];
+    } else if (msg["msgType"] == "fixtureProfiles") {
+        app.fixtureProfiles = msg["profiles"];
+    }
+});
