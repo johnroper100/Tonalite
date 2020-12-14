9fe0fc72bb991a82311a81a5d1c5a54fc0325ad6
John Roper <johnroper100@gmail.com>
2020-12-14 00:18:42 -0500

    mopre image work

--- resources/importimages.py
+++ resources/importimages.py
@@ -19,3 +19,25 @@ with open('IMAGES.idx', 'r') as ff:
 for image in images:
     with open('../fixture-images/'+image[0]+".png", 'wb') as f:
         f.write(bytes(image[2]))
+
+images = []
+last = 0
+data = None
+with open('CSEDIT_IMAGES.dat', 'rb') as f:
+    data = bytearray(f.read())
+with open('CSEDIT_IMAGES.idx', 'r') as ff:
+    lines = ff.readlines()
+    for l in range(1, len(lines)):
+        li = lines[l].split(",")
+        lr = lines[l-1].split(",")
+        image = [lr[0], lr[3], None]
+        image[2] = data[last+80:int(li[1])]
+        last = int(li[1])
+        images.append(image)
+
+for image in images:
+    if not os.path.exists('../fixture-images/'+image[0]+".png"):
+        with open('../fixture-images/'+image[0]+".png", 'wb') as f:
+            f.write(bytes(image[2]))
+    else:
+        print('../fixture-images/'+image[0]+".png")
