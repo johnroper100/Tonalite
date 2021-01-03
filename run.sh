fef5bc93fc493374a2ff607017ad582d3fd92d59
John Roper <johnroper100@gmail.com>
2021-01-02 19:49:59 -0500

    add other show formats

--- run.sh
+++ run.sh
@@ -3,6 +3,21 @@ ola_patch -d 2 -p 1 -u 2
 ola_patch -d 2 -p 2 -u 3
 ola_patch -d 2 -p 3 -u 4
 
+ola_patch -d 3 -p 0 -u 1
+ola_patch -d 3 -p 1 -u 2
+ola_patch -d 3 -p 2 -u 3
+ola_patch -d 3 -p 3 -u 4
+
+ola_patch -d 4 -p 0 -u 1
+ola_patch -d 4 -p 1 -u 2
+ola_patch -d 4 -p 2 -u 3
+ola_patch -d 4 -p 3 -u 4
+
+ola_patch -d 6 -p 0 -u 1
+ola_patch -d 6 -p 1 -u 2
+ola_patch -d 6 -p 2 -u 3
+ola_patch -d 6 -p 3 -u 4
+
 ola_patch -d 7 -p 0 -u 1
 ola_patch -d 7 -p 1 -u 2
 ola_patch -d 7 -p 2 -u 3
